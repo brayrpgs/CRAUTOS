9e77924409da1f2f09624386e5c081b393c0ce63
BrayRPGs <brayanymelquisedec@gmail.com>
2025-09-17 20:43:58 -0600

    Add Background component and update index.astro to use it

---
+++ src/components/background/Background.tsx
@@ -0,0 +1,16 @@
+const Backgroung = () => {
+    return <>
+        <div id="background-carrusel">
+            <img src="ram1.avif" alt="" />
+            <img src="ram2.avif" alt="" />
+            <img src="ram3.avif" alt="" />
+            <img src="toyota-hilux-rad-1.avif" alt="" />
+            <img src="toyota-hilux-rad-2.avif" alt="" />
+            <img src="toyota-hilux-rad-3.avif" alt="" />
+            <img src="toyota-hilux-rad-4.avif" alt="" />
+            <img src="ram1.avif" alt="" />
+        </div>
+    </>
+}
+
+export { Backgroung }
