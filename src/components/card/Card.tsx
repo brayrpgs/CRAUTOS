16e1d6fcb0054d704341d2664073841adb35edf3
BrayRPGs <brayanymelquisedec@gmail.com>
2025-10-02 17:57:02 -0600

    feat: implement Card component with styles and integrate into test page

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,3 +1,23 @@
-const Card = () => {
+import React from 'react'
 
+interface CardProps {
+  image: string
+  info: string
 }
+
+const Card: React.FC<CardProps> = (data: CardProps) => {
+  return (
+    <>
+      <div className='container-card size-card'>
+        <div className='image size-card'>
+          <img src={data.image} alt='' />
+        </div>
+        <p className='info size-card'>
+          {data.info}
+        </p>
+      </div>
+    </>
+  )
+}
+
+export { Card }
